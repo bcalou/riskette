3945d8135ae96b0c10d883cab9e8ac00cb50c3d3
bcalou <bastien.calou@gmail.com>
2022-03-02 19:22:27 +0100

    [dartboard] code review

--- src/lib/DartBoard.svelte
+++ src/lib/DartBoard.svelte
@@ -1,27 +1,31 @@
 <script lang="ts">
   // Physical Radius of a dart board : 457mm
   // Dimensions are in mm
+  // All radiuses are the outer bounds
   // from https://www.dimensions.com/element/dartboard
   const targetRadius = 451 / 2;
-  const doubleRadius = 170;
-  const exteriorSimpleRadius = doubleRadius - 8;
-  const trebleRadius = 107;
-  const interiorSimpleRadius = trebleRadius - 8;
+  const doubleRingRadius = 170;
+  const outerSimpleRadius = doubleRingRadius - 8;
+  const trebleRingRadius = 107;
+  const innerSimpleRadius = trebleRingRadius - 8;
   const bullRadius = 32 / 2;
   const bullsEyeRadius = 12.7 / 2;
 
-  const doubleTrebleColors = ["red", "green"];
+  const ringsColors = ["red", "green"];
   const simpleColors = ["black", "white"];
+  const bullColor = ringsColors[1];
+  const bullsEyeColor = ringsColors[0];
+  const dividerColor = "black";
 
-  //numbers clockwise from 20
-  const segments = [
+  // Numbers clockwise from 20
+  const sections = [
     20, 1, 18, 4, 13, 6, 10, 15, 2, 17, 3, 19, 7, 16, 8, 11, 14, 9, 12, 5,
   ];
 
-  //helpers
-  const nbSegments = segments.length;
-  const segmentArcRad = (Math.PI * 2) / nbSegments;
-  const segmentArcOffsetRad = -(Math.PI * 2) - segmentArcRad / 2;
+  // Helpers
+  const nbSections = sections.length;
+  const sectionArcRad = (Math.PI * 2) / nbSections;
+  const firstSectionArcOffsetRad = -(Math.PI * 2) - sectionArcRad / 2;
 
   function onBullsEyeClick() {
     console.log("bulls eye click");
@@ -32,119 +36,114 @@
   viewBox="{-targetRadius} {-targetRadius} {targetRadius * 2} {targetRadius *
     2}"
 >
+  <!-- Target border -->
   <path
     d="
-    m -{targetRadius}, 0
+    M -{targetRadius}, 0
     a {targetRadius},{targetRadius} 0 1,1 {targetRadius * 2},0
     a {targetRadius},{targetRadius} 0 1,1 -{targetRadius * 2},0
     z
-    M -{doubleRadius}, 0
-    a {doubleRadius},{doubleRadius} 0 1,0 {doubleRadius * 2},0
-    a {doubleRadius},{doubleRadius} 0 1,0 -{doubleRadius * 2},0
-    Z
-    "
-  />
-  <path
-    d="
-    m -{bullRadius}, 0
-    a {bullRadius},{bullRadius} 0 1,1 {bullRadius * 2},0
-    a {bullRadius},{bullRadius} 0 1,1 -{bullRadius * 2},0
-    z
-    M -{bullsEyeRadius}, 0
-    a {bullsEyeRadius},{bullsEyeRadius} 0 1,0 {bullsEyeRadius * 2},0
-    a {bullsEyeRadius},{bullsEyeRadius} 0 1,0 -{bullsEyeRadius * 2},0
+    M -{doubleRingRadius}, 0
+    a {doubleRingRadius},{doubleRingRadius} 0 1,0 {doubleRingRadius * 2},0
+    a {doubleRingRadius},{doubleRingRadius} 0 1,0 -{doubleRingRadius * 2},0
     Z
     "
   />
 
-  {#each segments as segmentNumber, segmentPosition}
-    <!-- compute colors -->
-    {@const doubleTrebleColor = doubleTrebleColors[segmentPosition % 2]}
-    {@const simpleColor = simpleColors[segmentPosition % 2]}
-    <!-- compute positions -->
+  {#each sections as sectionValue, sectionIndex}
+    <!-- Compute colors -->
+    {@const doubleTrebleColor = ringsColors[sectionIndex % 2]}
+    {@const simpleColor = simpleColors[sectionIndex % 2]}
+
+    <!-- Compute positions -->
     {@const leftBorderAxisX = Math.cos(
-      segmentPosition * segmentArcRad - segmentArcOffsetRad
+      sectionIndex * sectionArcRad - firstSectionArcOffsetRad
     )}
     {@const leftBorderAxisY = Math.sin(
-      segmentPosition * segmentArcRad - segmentArcOffsetRad
+      sectionIndex * sectionArcRad - firstSectionArcOffsetRad
     )}
     {@const rightBorderAxisX = Math.cos(
-      (segmentPosition + 1) * segmentArcRad - segmentArcOffsetRad
+      (sectionIndex + 1) * sectionArcRad - firstSectionArcOffsetRad
     )}
     {@const rightBorderAxisY = Math.sin(
-      (segmentPosition + 1) * segmentArcRad - segmentArcOffsetRad
+      (sectionIndex + 1) * sectionArcRad - firstSectionArcOffsetRad
     )}
-    <!-- treble -->
+
+    <!-- Treble ring area -->
     <path
       d="
-        M {leftBorderAxisX * doubleRadius},{leftBorderAxisY * doubleRadius}
-        A {doubleRadius},{doubleRadius} 0 0,1 {rightBorderAxisX *
-        doubleRadius},{rightBorderAxisY * doubleRadius}
-        L {rightBorderAxisX * exteriorSimpleRadius},{rightBorderAxisY *
-        exteriorSimpleRadius}
-        A {exteriorSimpleRadius},{exteriorSimpleRadius} 0 0,0 {leftBorderAxisX *
-        exteriorSimpleRadius},{leftBorderAxisY * exteriorSimpleRadius}
-        L {leftBorderAxisX * doubleRadius},{leftBorderAxisY * doubleRadius}
+        M {leftBorderAxisX * doubleRingRadius},{leftBorderAxisY * doubleRingRadius}
+        A {doubleRingRadius},{doubleRingRadius} 0 0,1 {rightBorderAxisX *
+        doubleRingRadius},{rightBorderAxisY * doubleRingRadius}
+        L {rightBorderAxisX * outerSimpleRadius},{rightBorderAxisY *
+        outerSimpleRadius}
+        A {outerSimpleRadius},{outerSimpleRadius} 0 0,0 {leftBorderAxisX *
+        outerSimpleRadius},{leftBorderAxisY * outerSimpleRadius}
+        L {leftBorderAxisX * doubleRingRadius},{leftBorderAxisY * doubleRingRadius}
         Z
         "
-      stroke="black"
+      stroke={dividerColor}
       fill={doubleTrebleColor}
     />
-    <!-- exterior simple -->
+
+    <!-- Outer simple area -->
     <path
       d="
-        M {leftBorderAxisX * exteriorSimpleRadius},{leftBorderAxisY *
-        exteriorSimpleRadius}
-        A {exteriorSimpleRadius},{exteriorSimpleRadius} 0 0,1 {rightBorderAxisX *
-        exteriorSimpleRadius},{rightBorderAxisY * exteriorSimpleRadius}
-        L {rightBorderAxisX * trebleRadius},{rightBorderAxisY * trebleRadius}
-        A {trebleRadius},{trebleRadius} 0 0,0 {leftBorderAxisX *
-        trebleRadius},{leftBorderAxisY * trebleRadius}
-        L {leftBorderAxisX * exteriorSimpleRadius},{leftBorderAxisY *
-        exteriorSimpleRadius}
+        M {leftBorderAxisX * outerSimpleRadius},{leftBorderAxisY *
+        outerSimpleRadius}
+        A {outerSimpleRadius},{outerSimpleRadius} 0 0,1 {rightBorderAxisX *
+        outerSimpleRadius},{rightBorderAxisY * outerSimpleRadius}
+        L {rightBorderAxisX * trebleRingRadius},{rightBorderAxisY * trebleRingRadius}
+        A {trebleRingRadius},{trebleRingRadius} 0 0,0 {leftBorderAxisX *
+        trebleRingRadius},{leftBorderAxisY * trebleRingRadius}
+        L {leftBorderAxisX * outerSimpleRadius},{leftBorderAxisY *
+        outerSimpleRadius}
         Z
         "
-      stroke="black"
+      stroke={dividerColor}
       fill={simpleColor}
     />
-    <!-- interior simple -->
+
+    <!-- Double ring area -->
     <path
       d="
-        M {leftBorderAxisX * trebleRadius},{leftBorderAxisY * trebleRadius}
-        A {trebleRadius},{trebleRadius} 0 0,1 {rightBorderAxisX *
-        trebleRadius},{rightBorderAxisY * trebleRadius}
-        L {rightBorderAxisX * interiorSimpleRadius},{rightBorderAxisY *
-        interiorSimpleRadius}
-        A {interiorSimpleRadius},{interiorSimpleRadius} 0 0,0 {leftBorderAxisX *
-        interiorSimpleRadius},{leftBorderAxisY * interiorSimpleRadius}
-        L {leftBorderAxisX * trebleRadius},{leftBorderAxisY * trebleRadius}
+        M {leftBorderAxisX * trebleRingRadius},{leftBorderAxisY * trebleRingRadius}
+        A {trebleRingRadius},{trebleRingRadius} 0 0,1 {rightBorderAxisX *
+        trebleRingRadius},{rightBorderAxisY * trebleRingRadius}
+        L {rightBorderAxisX * innerSimpleRadius},{rightBorderAxisY *
+        innerSimpleRadius}
+        A {innerSimpleRadius},{innerSimpleRadius} 0 0,0 {leftBorderAxisX *
+        innerSimpleRadius},{leftBorderAxisY * innerSimpleRadius}
+        L {leftBorderAxisX * trebleRingRadius},{leftBorderAxisY * trebleRingRadius}
         Z
         "
-      stroke="black"
+      stroke={dividerColor}
       fill={doubleTrebleColor}
     />
-    <!-- interior simple -->
+
+    <!-- Inner simple area -->
     <path
       d="
-        M {leftBorderAxisX * interiorSimpleRadius},{leftBorderAxisY *
-        interiorSimpleRadius}
-        A {interiorSimpleRadius},{interiorSimpleRadius} 0 0,1 {rightBorderAxisX *
-        interiorSimpleRadius},{rightBorderAxisY * interiorSimpleRadius}
+        M {leftBorderAxisX * innerSimpleRadius},{leftBorderAxisY *
+        innerSimpleRadius}
+        A {innerSimpleRadius},{innerSimpleRadius} 0 0,1 {rightBorderAxisX *
+        innerSimpleRadius},{rightBorderAxisY * innerSimpleRadius}
         L {rightBorderAxisX * bullRadius},{rightBorderAxisY * bullRadius}
         A {bullRadius},{bullRadius} 0 0,0 {leftBorderAxisX *
         bullRadius},{leftBorderAxisY * bullRadius}
-        L {leftBorderAxisX * interiorSimpleRadius},{leftBorderAxisY *
-        interiorSimpleRadius}
+        L {leftBorderAxisX * innerSimpleRadius},{leftBorderAxisY *
+        innerSimpleRadius}
         Z
         "
-      stroke="black"
+      stroke={dividerColor}
       fill={simpleColor}
     />
   {/each}
 
+  <!-- Bull -->
   <path
     d="
-      m -{bullRadius}, 0
+      M -{bullRadius}, 0
       a {bullRadius},{bullRadius} 0 1,1 {bullRadius * 2},0
       a {bullRadius},{bullRadius} 0 1,1 -{bullRadius * 2},0
       z
@@ -153,9 +152,11 @@
       a {bullsEyeRadius},{bullsEyeRadius} 0 1,0 -{bullsEyeRadius * 2},0
       Z
       "
-    fill="green"
+      fill={bullColor}
   />
-  <circle r={bullsEyeRadius} fill="red" on:click={onBullsEyeClick} />
+
+  <!-- Bull's eye -->
+  <circle r={bullsEyeRadius} fill={bullsEyeColor} on:click={onBullsEyeClick} />
 </svg>
 
 <style>
